18d42cc4add590f2043ada0dcc3790c79ccd5eb2
Joshua  Manalo <63303459+jmanalo26@users.noreply.github.com>
2022-01-17 15:58:14 -0600

    Time: 0 ms, Memory: 37.9 MB - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,18 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0){
+        if (n == 0)
             return 0;
-        }
         
-        if (n==1 || n==2){
+        if(n == 1 || n == 2)
             return n;
-        }
         
-        int[] stairs = new int[n+1];
+        int[] stairs = new int[n + 1];
         stairs[0] = 0;
         stairs[1] = 1;
         stairs[2] = 2;
-        
         for (int i = 3; i <= n; i++){
             stairs[i] = stairs[i-1] + stairs[i-2];
         }
